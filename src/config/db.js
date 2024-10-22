2fefcd9dc47fd8c982bef289c0065553ac86efec
Priscila <pyea08@gmail.com>
2024-10-22 12:37:49 -0300

    update

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(`${process.env.URI}`);
+        await mongoose.connect(`mongodb+srv://reksuat:rZcx7lfHX1TBhTrU@cluster0.tou9m.mongodb.net/oficinas`)
 
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
