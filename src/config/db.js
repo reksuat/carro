d5f88e0c90dbbe66b0a66f1ba20162cb602036d1
Priscila <pyea08@gmail.com>
2024-10-22 12:06:24 -0300

    URI

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect( "mongodb://localhost:27017/banco");
+        await mongoose.connect(`${process.env.URI}`);
 
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
